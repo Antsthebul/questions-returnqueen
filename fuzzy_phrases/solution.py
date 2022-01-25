0b02fb3c60392d7f0b264c2b4553497ae46a7d43
Antsthebul <anthony.allen.srt@gmail.com>
2022-01-25 16:43:26 -0500

    solved problem

--- fuzzy_phrases/solution.py
+++ fuzzy_phrases/solution.py
@@ -1,8 +1,31 @@
 import json
+import re
+
+pattern_segment = r'\s*\w*\s+'
 
 def phrasel_search(P, Queries):
     # Write your solution here
     ans = [[]]
+    for ij, sent in enumerate(Queries):
+        result = []
+        for phrase in P:
+            temp_str = ""
+            # retriece each wordf
+            phrase_count = phrase.split(" ")
+            for ix, word in enumerate(phrase.split(" ")):
+                if ix < len(phrase_count)-1:
+                    temp_str += word + pattern_segment
+                else:
+                    temp_str += word
+            match = re.findall(temp_str, sent)
+            if match:
+                if len(ans) == 0:
+                    ans[0] = match
+                else:
+                    try:
+                        ans[ij] += match
+                    except IndexError:
+                        ans.append(match)
     return ans
 
 if __name__ == "__main__":
